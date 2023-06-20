8808175a24747127fc6a8815825d5613470fe8a4
xiaochong0302 <xiaochong0302@gmail.com>
2023-06-20 16:43:41 +0800

    文件重命名

--- get-docker.sh
+++ get-docker.sh
@@ -5,8 +5,8 @@ set -e
 # See https://docs.docker.com/engine/install/ for the installation steps.
 #
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://get.docker.com -o get_docker.sh
-#   $ sh get_docker.sh
+#   $ curl -fsSL https://get.docker.com -o get-docker.sh
+#   $ sh get-docker.sh
 #
 # For test builds (ie. release candidates):
 #   $ curl -fsSL https://test.docker.com -o test-docker.sh
